cfcc1026579546b975dfed4e87af46e162e3966f
Sagynbek <kenzhebaevsagynbek@gmail.com>
2020-07-01 12:14:26 +0300

    new string functions added

--- src/functions/string.ts
+++ src/functions/string.ts
@@ -24,3 +24,58 @@ export const seperateJoinedWords = (target: string, seperator: string) => {
 
   return result;
 }
+
+export interface IParagraphyJoinedWordsConfig {
+  isCapitalizeDisabled?: boolean,
+}
+/** 
+ * camelCasedText => Camel cased text
+ * helloWorld! => Hello world!
+*/
+export const paragraphyJoinedWords = (target: string, config: IParagraphyJoinedWordsConfig = {}) => {
+  if (!target) { return ""; }
+
+  let result = "";
+  for (let it = 0; it < target.length; it++) {
+    result += isUpperCase(target[it]) ?
+      " " + target[it].toLowerCase()
+      : target[it];
+  }
+  result = result.trim();
+  result = config.isCapitalizeDisabled !== true && result ? (result[0].toUpperCase() + result.slice(1)) : result;
+
+  return result;
+}
+
+
+/** 
+ * Upper Cased Text => Upper cased text
+ * Long Paragraph. With 2 Sentences  => Long paragraph. With 2 senteces
+*/
+export const fixParagraphCase = (target: string) => {
+  if (!target) { return ""; }
+
+  const regexForEndOfSentence = new RegExp("[^a-zA-Z0-9,;:\s\"']")
+
+  let result = "";
+  for (let it = 0; it < target.length; it++) {
+    if (isUpperCase(target[it])
+      && (
+        it === 0
+        || (it - 1 >= 0 && target[it - 1].match(regexForEndOfSentence)) // New sentence without space break
+        || (it - 2 >= 0 && target[it - 1] === "" && target[it - 2].match(regexForEndOfSentence)) // If new sentence
+        || (it + 1 < target.length && isUpperCase(target[it + 1])) // If consecutively upperCases
+        || (it - 1 >= 0 && isUpperCase(target[it - 1])) // If consecutively upperCases
+      )
+    ) {
+      result += target[it];
+    }
+    else {
+      result += target[it].toLowerCase();
+    }
+  }
+  result = result.trim();
+  result = result ? (result[0].toUpperCase() + result.slice(1)) : result;
+
+  return result;
+}
--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,16 @@
 import { isNotNullOrUndefined, isNullOrUndefined } from './functions/checker';
-import { isUpperCase, isLowerCase, seperateJoinedWords } from './functions/string';
+import { isUpperCase, isLowerCase, seperateJoinedWords, paragraphyJoinedWords, fixParagraphCase } from './functions/string';
 import { each } from './functions/common';
 
 
 export default {
   isNotNullOrUndefined, isNullOrUndefined,
-  isUpperCase, isLowerCase, seperateJoinedWords,
+  isUpperCase, isLowerCase, seperateJoinedWords, paragraphyJoinedWords, fixParagraphCase,
   each,
 }
 export {
   isNotNullOrUndefined, isNullOrUndefined,
-  isUpperCase, isLowerCase, seperateJoinedWords,
+  isUpperCase, isLowerCase, seperateJoinedWords, paragraphyJoinedWords, fixParagraphCase,
   each,
 }
+
